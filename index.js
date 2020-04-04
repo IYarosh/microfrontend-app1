ff363ab58de16809cd2f507fe3f37359dec9d7ee
IYarosh <IYarosh@qulix.com>
2020-04-04 14:30:23 +0300

    Add single-spa hook functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Test function from microfrontend app1');
+console.log('Test log from microfrontend app1');
+
+let domElement;
+
+export function bootstrap(props) {
+    console.log('App 1 bootstrap. Props:', props)
+    return Promise
+        .resolve()
+        .then(() => {
+            domElement = document.createElement('div');
+            domElement.id = 'app1';
+            document.body.appendChild(domElement);
+        })
+}
+
+export function mount(props) {
+    console.log('App 1 mount. Props:', props)
+    return Promise
+        .resolve()
+        .then(() => {
+            domElement.textContent = 'App 1 is mounted!'
+        })
+}
+
+export function unmount(props) {
+    console.log('App 1 unmount. Props:', props)
+    return Promise
+        .resolve()
+        .then(() => {
+            domElement.textContent = '';
+        })
+}
